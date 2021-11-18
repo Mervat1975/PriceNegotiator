44d8224a6cb06846dc3bc9d88c9e25f5feccd547
Mervat1975 <mervat.mustafa@dcmail.ca>
2021-11-17 21:54:28 -0500

    fixed ai

--- ai/components/chatbot.py
+++ ai/components/chatbot.py
@@ -1,79 +1,90 @@
+import os
+import random
+import json
+from keras.models import load_model
+import numpy as np
+import pickle
 import nltk
 from nltk.stem import WordNetLemmatizer
-import pickle
-import numpy as np
-import os
-
-import json
-import random
-
-from tensorflow.keras.models import load_model
-
-absolutepath = os.path.abspath(__file__)
-fileDirectory = os.path.dirname(absolutepath)
-parentDirectory = os.path.dirname(fileDirectory)
-newPath = os.path.join(parentDirectory, 'components')
-
-model = load_model(os.path.join(newPath, 'model'))
-
-intents = json.loads(open(os.path.join(newPath, 'intents.json')).read())
-words = pickle.load(open(os.path.join(newPath, 'words.pkl'), 'rb'))
-classes = pickle.load(open(os.path.join(newPath, 'classes.pkl'), 'rb'))
-
 lemmatizer = WordNetLemmatizer()
 
 
+model = load_model(os.path.join("./ai/components", 'chatbot_model.h5'))
+intents = json.loads(
+    open(os.path.join("./ai/components", 'intents.json')).read())
+words = pickle.load(open(os.path.join("./ai/components", 'words.pkl'), 'rb'))
+classes = pickle.load(
+    open(os.path.join("./ai/components", 'classes.pkl'), 'rb'))
+
+print('words', words)
+print('classes', classes)
+
+
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
+    # tokenize the pattern - splitting words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    # stemming every word - reducing to base form
+    sentence_words = [lemmatizer.lemmatize(
+        word.lower()) for word in sentence_words]
     return sentence_words
 
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
-def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+# return bag of words array: 0 or 1 for words that exist in sentence
+def bag_of_words(sentence, words, show_details=True):
+    # tokenizing patterns
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0] * len(words)
+    # bag of words - vocabulary matrix
+    bag = [0]*len(words)
     for s in sentence_words:
-        for i, w in enumerate(words):
-            if w == s:
+        for i, word in enumerate(words):
+            if word == s:
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print("found in bag: %s" % w)
-    return np.array(bag)
+                    print("found in bag: %s" % word)
+    print("np.array(bag)", np.array(bag))
+    return(np.array(bag))
 
 
-def predict_class(sentence, model):
-    # filter out predictions below a threshold
-    p = bow(sentence, words, show_details=False)
-    res = model.predict(np.array([p]))[0]
-    error_threshold = 0.25
-    results = [[i, r] for i, r in enumerate(res) if r > error_threshold]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
+def predict_class(sentence):
+    # filter below  threshold predictions
+    p = bag_of_words(sentence, words, show_details=True)
+    flag = False
     return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    for x in p:
+        if x == 1:
+            flag = True
+    if (flag == True):
+        res = model.predict(np.array([p]))[0]
+        ERROR_THRESHOLD = 0.25
+        results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+        # sorting strength probability
+        results.sort(key=lambda x: x[1], reverse=True)
+
+        print("result", results)
+        for r in results:
+            return_list.append(
+                {"intent": classes[r[0]], "probability": str(r[1])})
+    else:
+        return_list.append({"intent": "noanswer", "probability": str(1)})
+
     return return_list
 
 
-def get_response(ints, intents_json):
+def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
-    result = 'try'
     for i in list_of_intents:
-        if i['tag'] == tag:
+        if(i['tag'] == tag):
             result = random.choice(i['responses'])
             break
     return result
 
 
 def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = get_response(ints, intents)
+
+    ints = predict_class(msg)
+    print('ints', ints)
+    res = getResponse(ints, intents)
+    print('res', res)
     return res
--- ai/components/train_chatbot.py
+++ ai/components/train_chatbot.py
@@ -1,43 +1,37 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-
-import json
 import pickle
-
+import json
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+#from keras.optimizers import SGD
 from tensorflow.keras.optimizers import SGD
 import random
 
-nltk.download()
-
+import nltk
+from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
 words = []
 classes = []
 documents = []
-ignore_words = ['?', '!']
-data_file = open('intents.json').read()
-intents = json.loads(data_file)
+ignore_letters = ['!', '?', ',', '.']
+intents_file = open('intents.json').read()
+intents = json.loads(intents_file)
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-
         # tokenize each word
-        w = nltk.word_tokenize(pattern)
-        words.extend(w)
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
         # add documents in the corpus
-        documents.append((w, intent['tag']))
-
+        documents.append((word, intent['tag']))
         # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-
-# https://github.com/balajisrinivas/ChatBot-using-Keras-TkinterGUI
-# https://heartbeat.fritz.ai/building-a-conversational-chatbot-with-nltk-and-tensorflow-part-1-f452ce1756e5
+print(documents)
 # lemmaztize and lower each word and remove duplicates
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = [lemmatizer.lemmatize(w.lower())
+         for w in words if w not in ignore_letters]
 words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
@@ -62,10 +56,11 @@ for doc in documents:
     # list of tokenized words for the pattern
     pattern_words = doc[0]
     # lemmatize each word - create base word, in attempt to represent related words
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    pattern_words = [lemmatizer.lemmatize(
+        word.lower()) for word in pattern_words]
     # create our bag of words array with 1, if word match found in current pattern
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
+    for word in words:
+        bag.append(1) if word in pattern_words else bag.append(0)
 
     # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
@@ -91,10 +86,12 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd, metrics=['accuracy'])
 
 # fitting and saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('model', hist)
+hist = model.fit(np.array(train_x), np.array(train_y),
+                 epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
 
-print("model created and saved")
+print("model created")
